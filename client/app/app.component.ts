e5339c69814452e2c4785d20e24c951b8f6906ec
vamsi.achieve.target@gmail.com <vamsi.achieve.target@gmail.com>
2016-11-21 23:36:03 +0530

    Update app Component

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 import {TaskService} from './services/task.service';
 
 @Component({
-  moduleId: module.id,
   selector: 'my-app',
-  templateUrl: 'app.component.html',
+  templateUrl: './app/app.component.html',
   providers:[TaskService]
 })
 
